efbde72b17b49bdcac90c4e6cb55e732e0f5c2b6
Lucas M <120651078+lucasm6195@users.noreply.github.com>
2025-09-18 17:57:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
@@ -52,7 +52,7 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist', 
     },
     server: {
       port: 3000,
